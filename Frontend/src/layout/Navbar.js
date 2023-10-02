fb542ff13c0fc2c89037f341a8ab829941e277f5
tusharDeomare01 <125716161+tusharDeomare01@users.noreply.github.com>
2023-10-02 20:06:19 +0530

    Update Navbar.js

--- Frontend/src/layout/Navbar.js
+++ Frontend/src/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//This Component Contains the UI of NavigationBar
 export default function Navbar() {
   return (
     <div>
@@ -21,6 +22,7 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
 
+          /*It's a button to Add a New Task*/
           <Link className="btn btn-outline-light" to="/addtask">
             Add Task
           </Link>
