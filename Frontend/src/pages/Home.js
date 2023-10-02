064ad50fc15364e23e4f9109eacb23d36e2916ae
tusharDeomare01 <125716161+tusharDeomare01@users.noreply.github.com>
2023-10-02 20:14:33 +0530

    Update Home.js

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
+//This Component represent the HomePage of WebApllication
 export default function Home() {
   const [users, setUsers] = useState([]);
 
@@ -11,6 +12,9 @@ export default function Home() {
     loadUsers();
   }, []);
 
+  //axios : It's used to Connect to the BackEnd Server.
+  //async : Async methods are often used to make network requests, such as fetching data from an API.
+  //await : await is usually used to unwrap promises by passing a Promise as the expression.
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:8080/taskdetails");
     setUsers(result.data);
@@ -21,7 +25,7 @@ export default function Home() {
     loadUsers();
   };
 
-  return (
+  return (          //The return Statement represent the table of the Task which Contain info of Task such as ID, Title, EndDate, Status etc.
     <div className="container">
       <div className="py-4">
         <table className="table border shadow">
@@ -44,6 +48,7 @@ export default function Home() {
                 <td>{user.endDate}</td>
                 <td>{user.status}</td>
                 <td>
+              /*Below are 3 links tags which are used to view Task info, Edit task info and Delete a tasl*/
                   <Link
                     className="btn btn-primary mx-2"
                     to={`/viewtask/${user.id}`}
