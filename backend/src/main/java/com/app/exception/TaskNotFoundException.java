2f30695b8280a150070e6dcb8bbdcf86d79ab63a
tusharDeomare01 <125716161+tusharDeomare01@users.noreply.github.com>
2023-10-02 20:01:44 +0530

    Update TaskNotFoundException.java

--- backend/src/main/java/com/app/exception/TaskNotFoundException.java
+++ backend/src/main/java/com/app/exception/TaskNotFoundException.java
@@ -2,6 +2,7 @@ package com.app.exception;
 
 public class TaskNotFoundException extends RuntimeException{
 
+	//Here One Constructor is Made which show the Exception "TaskNotFound".
 	public TaskNotFoundException(Long id) {
 		super("Task Not Found with id "+ id);
 	}
